fa9f25df88dbd79f3a31276bbe1db4e924fa5b9b
Sparsh5107 <sparshfulzele512007@gmail.com>
2026-07-01 00:23:24 +0530

    Work on Navbar 01

---
+++ src/components/sparsh-folder/NavBar_component/navdata.js
@@ -0,0 +1,92 @@
+export const industries = [
+  {
+    title: "Banking & Financial Services",
+    href: "/industries/banking-financial-services",
+  },
+  {
+    title: "Healthcare",
+    href: "/industries/healthcare",
+  },
+  {
+    title: "Education",
+    href: "/industries/education",
+  },
+  {
+    title: "Travel & Logistics",
+    href: "/industries/travel-logistics",
+  },
+  {
+    title: "Hospitality",
+    href: "/industries/hospitality",
+  },
+  {
+    title: "Public Service",
+    href: "/industries/public-service",
+  },
+  {
+    title: "High Tech",
+    href: "/industries/high-tech",
+  },
+  {
+    title: "Manufacturing",
+    href: "/industries/manufacturing",
+  },
+  {
+    title: "Retail",
+    href: "/industries/retail",
+  },
+  {
+    title: "Insurance",
+    href: "/industries/insurance",
+  },
+];
+
+export const services = [
+  {
+    title: "Application Development",
+    href: "/services/application-development",
+  },
+  {
+    title: "Consulting",
+    href: "/services/consulting",
+  },
+  {
+    title: "Software Engineering",
+    href: "/services/software-engineering",
+  },
+  {
+    title: "Data & Analytics",
+    href: "/services/data-analytics",
+  },
+  {
+    title: "Artificial Intelligence",
+    href: "/services/artificial-intelligence",
+  },
+  {
+    title: "EdTech",
+    href: "/services/edtech",
+  },
+];
+
+export const navLinks = [
+  {
+    title: "Who We Are",
+    href: "/about",
+  },
+  {
+    title: "Industries",
+    dropdown: industries,
+  },
+  {
+    title: "Services",
+    dropdown: services,
+  },
+  {
+    title: "Career",
+    href: "/career",
+  },
+  {
+    title: "Contact",
+    href: "/contact",
+  },
+];
