dab47d6221662e772dfcb9dac7a26e60ed63f4ba
Sparsh5107 <sparshfulzele512007@gmail.com>
2026-07-11 11:17:11 +0530

    Carrer page done

---
+++ src/components/sparsh-folder/pages/Carrer/Carrer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CareerHero from './componets/CarrerHero'
+import CareerSections from './componets/CarrerSections'
+
+function Carrer() {
+    return (
+        <>
+            <CareerHero />
+            <CareerSections />
+
+        </>
+    )
+}
+
+export default Carrer
